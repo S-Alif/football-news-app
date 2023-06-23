f117b5c14bcb2ce9780d45b623d4d08b5458c346
Alif <79266506+S-Alif@users.noreply.github.com>
2023-06-23 20:23:58 +0600

    readme file added

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# UEFA Champions League Score website for season 2022-2023
+
+### Built with
+- Bootstrap 5.3
+- Javascript Plugins
+- Basic Javascript
--- js/script.js
+++
@@ -1,8 +0,0 @@
-let dataFetch = async () => {
-  fetch('https://apiv3.apifootball.com/?action=get_teams&league_id=302&APIkey=2a78ffbd3119513469a6e19b3d62a2a056386ef9aee80b7c7ca93090c2aadb06')
-    .then(res => res.json)
-    .then(data => console.log(data))
-    .catch(e => console.log(e))
-}
-
-dataFetch()
